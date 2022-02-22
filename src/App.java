29478c996a67011f94945bfabcf56a6b51e0c176
krishis2003 <k.zakrepskis@inbox.lv>
2022-02-22 13:29:46 +0200

    pievienoju cikluy for

--- src/App.java
+++ src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("gbnfgnfgnfgnfgnf");
+        for (int i = 0; i < 120; i++) {
+            System.out.println(i);
+        }
     }
 }
